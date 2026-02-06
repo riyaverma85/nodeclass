64eccf3ecaaa49f59e97f3dc6bc77499595f504b
riyaverma85 <riyaverma2976@gmail.com>
2026-02-06 11:22:40 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,89 @@
+// const http = require('http');
+// const fs = require('fs');
+// http.createServer((req, res)=>{
+//     // res.write("<h1>welcome this page..........</h1>");
+//     // res.write("<h1> hello i am riya </h1>") 
+//     // res.end();
+
+//     fs.readFile('rah.txt',(err,data)=>{
+
+//         if(err)throw err;
+//         res.write(data);
+//         res.end();
+//     });
+// }).listen(8000 ,()=>{
+//     console.log("server is running on port 8000");
+// }); 
+
+
+// const http = require("http")
+// const fs = require("fs")
+
+// http.createServer((req,res)=>{
+
+
+//     fs.readFile("raj.txt",(err,data)=>{
+//         if(err)throw err;
+//         res.write
+//     })
+// }).listen(8000,()=>{
+//     console.log("server is running on port 8000")
+// });
+
+///////////////////////////new file/////////////////////////////////////////////////
+
+// const fs =require("fs")
+
+// fs.appendFile("ranu.txt","i am riyaaaa",(err)=>{
+//      if (err)throw err ;
+//      console.log("file created")
+// })
+
+
+//  const fs =require("fs");
+
+//  fs.open("riya.txt", "w","i am riyaaaa",(err,data)=>{
+//       if (err)throw err ;
+//       console.log("file created")
+//  })
+
+
+
+
+// const fs =require("fs");
+
+//  fs.writeFile("ayy.txt","i am rtklyjngvriyaaaa",(err)=>{
+//       if (err) throw err ;
+//       console.log("file created")
+//  })
+
+//////////////////////////////delete filee////////////////////////////////////
+
+// const fs =require("fs");
+
+//  fs.unlink("ayy.txt",(err)=>{
+//       if (err) throw err ;
+//       console.log("file deleted")
+//  })
+
+
+//////////////////////////////////// rename file ////////////////////////////////////////
+
+// const fs =require("fs");
+
+//  fs.rename("rah.txt","riya.txt",(err)=>{
+//       if (err) throw err ;
+//       console.log("file rename")
+//  })
+
+///////////////////////////////////////uppercase///////////////////////////////////////////////
+   
+
+    // const http = require("http");
+    // const uc = require("uppercase");
+
+    // http.createServer(function(req,res){
+    //     res.write(uc("welcome to my page"));
+    //     res.end();
+    // }).listen(8080)
+  
