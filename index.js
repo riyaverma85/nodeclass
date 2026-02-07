cc9659198318e96238074aa8bebc45626114914a
riyaverma85 <riyaverma2976@gmail.com>
2026-02-06 18:02:21 -0800

    .,,.

--- index.js
+++ index.js
@@ -93,9 +93,18 @@
 
   const express =require("express")
   const app = express();
+ 
+  app.get("/",(req,res)=>{
+    res.send("<h1>welcome to my page</h1>")
+  })
 
+app.get("/home",(req,res)=>{
+    res.send("<h1>this is home page</h1>")
+})
 
-
+app.get("/about",(req,res)=>{
+    res.send("<h1>this is about page</h1>")
+})
 
   app.listen(8000,()=>{
     console.log("server is running on port 8000")
